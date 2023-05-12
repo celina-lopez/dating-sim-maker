1146c5cb846f69c0fa3eb10628796012af105ab9
Celina <marisa.celina.lopez4@gmail.com>
2023-05-12 00:26:03 -0700

    basic player

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -4,29 +4,28 @@ class PlayController < ApplicationController
   layout "story"
 
   def index
+    chapter = @story.chapters.find_by_order_number(params[:current_chapter_index] || 0)
     @props = Jbuilder.new do |json|
-      json.story do
-        json.extract! @story, :title, :description
-        json.chapters @story.chapters.order(:order_number) do |chapter|
-          json.extract! chapter, :title, :background_image_url, :order_number
-          json.lines chapter.lines do |line|
-            json.extract! line, :line_type, :emotion, :text, :order
-            json.character_id line.speakeable_id
-            json.is_option line.option?
-            json.speaker_name line.speaker_name
-            json.mc line.mc?
-            json.option do
-              if line.option.present?
-                json.extract! line.option, :answers, :weights
-              end
+      json.extract! @story, :title, :description
+      json.chapter do
+        json.extract! chapter, :title, :background_image_url, :order_number
+        json.lines chapter.lines do |line|
+          json.extract! line, :line_type, :text, :order
+          character = line.mc? ?
+            Character.new(name: "MC", emotions: {}) : @story.characters.find(line.speakeable_id) 
+          json.character do
+            json.extract! character, :name
+            json.emotion_url character.emotions[line.emotion]
+          end
+          json.is_option line.option?
+          json.speaker_name line.speaker_name
+          json.option do
+            if line.option.present?
+              json.extract! line.option, :answers, :weights
             end
           end
         end
       end
-
-      json.characters @story.characters do |character|
-      end
-
     end.target!
   end
 
--- app/javascript/bundles/Play/components/Play.js
+++ app/javascript/bundles/Play/components/Play.js
@@ -1,18 +1,41 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
-export default function Play ({story, characters}) {
-  const [name, setName] = useState(story.title);
-
+export default function Play ({title, chapter}) {
+  const [currentLineIndex, setCurrentLine] = useState(-1);
+  const currentLine = chapter.lines[currentLineIndex];
+  const nextLine = useCallback(() => {
+    setCurrentLine((idx) => idx + 1)
+  },[])
+  
   return (
-    <div>
-      <h3>Hello, {name}!</h3>
-      <hr />
-      <form>
-        <label htmlFor="name">
-          Say hello to:
-          <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-      </form>
+    <div className='container'>
+      {title}
+      <div>{chapter.title}</div>
+      <div className='dating-sim' style={{backgroundImage: `url(${chapter.background_image_url})` }}>
+        {!currentLine
+          ? <div className='title-card'>
+            {currentLineIndex < 0
+              ? <div>
+                {chapter.title}
+                <div className='next' onClick={nextLine}>Begin</div>
+              </div> : "Fin"
+            }
+          </div>
+          : <div> 
+            <div className='conversation'>
+              <div className='line'>
+                <div className='name'>{currentLine.character.name}</div>
+                <div>{currentLine.text}</div>
+                <div className='next' onClick={nextLine}>Next</div>
+              </div>
+            </div>
+            <div className='row justify-content-between characters'>
+              <div className='col-5'><img src={currentLine.character.emotion_url} style={{width: "100%"}}/></div>
+              <div className='col-5'><img src={currentLine.character.emotion_url} style={{width: "100%"}}/></div>
+            </div>
+          </div>
+        }
+      </div>
     </div>
   );
 };
